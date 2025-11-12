5338f439c40477a15ab26e9a8a56d8cd0d67ffb9
Dusan-Milekic <dusanmilekic0511@gmail.com>
2025-11-12 10:37:14 +0100

    Kreiranje projekta
    Instaliranje potrebnih biblioteka i framewoorka za sam rad

---
+++ audiophile-ecommerce-project/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          strong: '#d87d4a',
+          light: '#fbaf85',
+        },
+        black: {
+          light: '#101010',
+          strong: '#000000',
+        },
+        grey: {
+          strong: '#f1f1f1',
+          light: '#fafafa',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
